915386f57f099d7c28ba12c604f8fe4e58d1b337
Born <sayavongsouvannachack@gmail.com>
2025-04-10 15:19:37 +0700

    Add lib_db file with database configuration94

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -7,9 +7,9 @@ const pool = new Pool({
   password: process.env.POSTGRESPASSWORD,
   port: parseInt(process.env.POSTGRESPORT || '5432'),
   connectionString: process.env.POSTGRESURL,
-  ssl: {
-    rejectUnauthorized: false // เปิดใช้งาน SSL และยอมรับการเชื่อมต่อที่ไม่ถูกต้อง
-  }
+  // ssl: {
+  //   rejectUnauthorized: false // เปิดใช้งาน SSL และยอมรับการเชื่อมต่อที่ไม่ถูกต้อง
+  // }
 });
 
 export default pool;
