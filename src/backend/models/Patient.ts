283928949a563974da2c786752d73d985f43876f
Born <sayavongsouvannachack@gmail.com>
2025-04-12 19:08:51 +0700

    Fix search by firstName and lastName

--- src/backend/models/Patient.ts
+++ src/backend/models/Patient.ts
@@ -30,7 +30,7 @@ export const PatientModel = {
         OR LOWER(last_name) LIKE LOWER($1)
         OR LOWER(middle_name) LIKE LOWER($1)
         OR phone_number ILIKE $1
-        OR LOWER(first_name || ' ' || last_name) LIKE LOWER($1)
+        OR LOWER(first_name || last_name) LIKE LOWER($1)
         OR LOWER(first_name || ' ' || middle_name || ' ' || last_name) LIKE LOWER($1)
       `;
       countQuery += ` WHERE ${searchCondition}`;
